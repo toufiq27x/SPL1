0ce57d2c5afe9d7a4ac633cd52cd4e69bbe53af8
toufiq27x <toufiqalam27x@gmail.com>
2026-01-11 20:24:10 +0600

    Initialize maze and player variables in pacman.cpp
    Added maze representation and player variables.

--- pacman.cpp
+++ pacman.cpp
@@ -1,15 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+#define CELL_SIZE 20
+
+#define WALL 1
+#define OBSTACLE 2
+#define DOT 3
+#define POWER_UP 4
+#define RED_ZONE 5
+#define EMPTY 0
 
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 700;
 
+vector<vector<int>> maze;
+int ROWS;
+int COLS;
+
+int playerX = 1;
+int playerY = 1;
+char playerDirection = 'd';
+
+int score = 0;
+int level = 1;
+bool gameOver = false;
+bool gameWon = false;
+
+int remainingDots = 0;
+
+sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "Pac-Man Game");
+sf::Font font;
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "Pac-Man Game");
-    sf::Font font;
-    
     if (!font.openFromFile("/System/Library/Fonts/Helvetica.ttc")) {
         if (!font.openFromFile("/System/Library/Fonts/Supplemental/Arial.ttf")) {
             if (!font.openFromFile("C:\\Windows\\Fonts\\arial.ttf")) {
