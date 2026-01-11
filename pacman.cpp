6e357eda73ce6f6f01a5f78f9b061df795bcff02
toufiq27x <toufiqalam27x@gmail.com>
2026-01-11 20:35:31 +0600

    Add maze loading functionality from file
    The function loads a maze from a specified file, handling errors for file access and empty content. It populates the maze structure based on characters read from the file.

--- pacman.cpp
+++ pacman.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -34,6 +36,55 @@ int remainingDots = 0;
 sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "Pac-Man Game");
 sf::Font font;
 
+void loadMazeFromFile(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error: Could not open file " << filename << endl;
+        exit(1);
+    }
+
+    vector<string> tempMaze;
+    string line;
+    
+    while (getline(file, line)) {
+        if (!line.empty() && line.back() == '\r') {
+            line.pop_back();
+        }
+        tempMaze.push_back(line);
+    }
+    file.close();
+
+    if (tempMaze.empty()) {
+        cout << "Error: Maze file is empty!" << endl;
+        exit(1);
+    }
+
+    ROWS = tempMaze.size();
+    COLS = 0;
+    for (const auto& row : tempMaze) {
+        if (row.size() > COLS) COLS = row.size();
+    }
+    
+    maze.resize(ROWS, vector<int>(COLS, EMPTY));
+    remainingDots = 0;
+    
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            char cell = (j < tempMaze[i].size()) ? tempMaze[i][j] : ' ';
+            
+            switch (cell) {
+                case '#': maze[i][j] = WALL; break;
+                case 'O': maze[i][j] = OBSTACLE; break;
+                case 'P': maze[i][j] = POWER_UP; break;
+                case '.': maze[i][j] = DOT; remainingDots++; break;
+                case 'E': maze[i][j] = EMPTY; break;
+                case ' ': maze[i][j] = EMPTY; break;
+                default: maze[i][j] = EMPTY; break;
+            }
+        }
+    }
+}
+
 int main() {
     if (!font.openFromFile("/System/Library/Fonts/Helvetica.ttc")) {
         if (!font.openFromFile("/System/Library/Fonts/Supplemental/Arial.ttf")) {
@@ -43,6 +94,8 @@ int main() {
             }
         }
     }
+    
+    loadMazeFromFile("easy_maze.txt");
     
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
